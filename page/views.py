3977adb080d6019e2126925ca5408350e87f5f70
HYUNJo3o <sodapop47@cau.ac.kr>
2020-08-04 03:34:00 +0900

    first commit of Dreamary

---
+++ page/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Designer
+
+
+# Create your views here.
+def home(request):
+    designers = Designer.objects.all()
+    return render(request, 'home.html', {'designers' : designers})
+
+def introduce(request):
+    return render(request, 'introduce.html')
